774d4650cc12ccf95899aa35e7aa439752738237
gebusstephane-bit <gebus.stephane@gmail.com>
2026-02-13 16:17:58 +0100

    🔧 fix: Disable outputFileTracing completely

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Désactiver complètement le output file tracing
+  outputFileTracing: false,
+  // Désactiver l'optimisation SWC qui cause des bugs
+  swcMinify: false,
   images: {
     remotePatterns: [
       {
@@ -14,24 +18,16 @@ const nextConfig = {
       },
     ],
   },
-  // Désactiver l'optimisation pour éviter le bug clientModules
-  swcMinify: false,
   experimental: {
+    // Désactiver les optimisations expérimentales problématiques
+    optimizeCss: false,
     serverComponentsExternalPackages: ['@supabase/supabase-js'],
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js',
     };
-    
-    // Désactiver le tracing côté client
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-      };
-    }
-    
     return config;
   },
 };
